55af8e5712c2b8af9faf373c14ef91c633e1dbc8
fasha1302 <fseck96@gmail.com>
2019-03-08 17:12:31 +0000

    priorityset added to notification creation

--- src/app/providers/priority.service.ts
+++ src/app/providers/priority.service.ts
@@ -4,12 +4,16 @@ import { map } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment.prod';
 import { Observable } from 'rxjs/internal/Observable';
+import { Priority } from '../interfaces/priority.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class PlantsService {
+export class PriorityService {
   headers = new HttpHeaders();
+  available = false;
+  prioritiesList:Priority[] = [];
 
   constructor(public http: HttpClient) { 
     this.headers.append("Accept","application/json");
@@ -21,9 +25,25 @@ export class PlantsService {
     return body || { };
   }
 
-  getAllPlants() : Observable<any>{
-    return this.http.get(`${environment.apiUrl}`+'PlanPlantSet',{headers:this.headers,responseType:'json'}).pipe(
+  getAllPriorities() : Observable<any>{
+    return this.http.get(`${environment.apiUrl}`+'PrioritySet',{headers:this.headers,responseType:'json'}).pipe(
       map(this.extractData)
     );
+  }
+
+  setPriorities(priorities){
+    if(priorities.length>0){
+      this.prioritiesList = priorities;
+      this.available = true;
+    }
+      
+  }
+
+  checkAvailability(){
+    return this.available;
+  }
+
+  getPriorities(){
+    return this.prioritiesList;
   }
 }
